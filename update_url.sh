1109367e0affcf0f6ea7f8d6c11e8bd677d51449
saransakthivelvisaiyon <saran.sakthivel@visaiyon.com>
2025-07-21 17:34:24 +0530

    url file updated

---
+++ update_url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+touch cloudflared_url.txt
+
+# Step 1: Run Cloudflared & capture URL
+( cloudflared tunnel --url http://localhost:8000 2>&1 | tee cloudflared.log | grep -m 1 "trycloudflare.com" | awk '{print $NF}' > cloudflared_url.txt ) &
+
+# Step 2: Wait for URL capture
+sleep 5
+
+# Step 3: Git push the updated URL
+git add cloudflared_url.txt
+git commit -m "Updated Cloudflared URL $(date)"
+git push origin main
+
+
